eababad3c4c86add2c52fba95f1accdfb1596574
ONEO <x@oneo.me>
2025-12-24 16:18:58 +0800

    Do not randomly update the page address

--- extension-env.d.ts
+++ extension-env.d.ts
@@ -3,7 +3,7 @@
 // If you need additional types, consider creating a new *.d.ts file and
 // referencing it in the "include" array of your tsconfig.json file.
 // See https://www.typescriptlang.org/tsconfig#include for more information.
-/// <reference types="extension/dist/types/index.d.ts" />
+/// <reference types="extension/types" />
 
-// Polyfill types for browser.* APIs.
-/// <reference types="extension/dist/types/polyfill.d.ts" />
+// Polyfill types for browser.* APIs
+/// <reference types="extension/types/polyfill" />
